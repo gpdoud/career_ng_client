f7e9b7719c4ab01fe9b70b653e699b6835ead305
gpdoud <gpdoud@gmail.com>
2023-05-18 13:30:28 -0400

    menu module and component

---
+++ src/app/menu/menu/menu.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Menu } from '../menu.class';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+
+  menus: Menu[] = [
+    new Menu("Test", ""),
+    new Menu("User", "/user/list"),
+    new Menu("Test", "")
+  ];
+  
+}
