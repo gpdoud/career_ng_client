b6387ee1eff56abcafb3486ddc96bdb1f103d0a5
Greg Doud <gpdoud@gmail.com>
2023-05-20 15:59:39 -0400

    Display logged in user lastname on menu

--- src/app/misc/menu/menu.component.ts
+++ src/app/misc/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Menu } from './menu.class';
+import { SystemService } from '../services/system.service';
 
 @Component({
   selector: 'app-menu',
@@ -12,5 +13,21 @@ export class MenuComponent {
     new Menu("User", "/user/list"),
     new Menu("Login", "/login"),
   ];
+  userDisplay: string = '[]';
+
+  constructor(
+    private sys: SystemService
+  ) {}
+
+  ngOnInit(): void {
+    if(this.sys.loggedInUser === null) {
+      this.userDisplay = '[Login]';
+    } else {
+      this.userDisplay = `[${this.sys.loggedInUser.lastname}]`;
+      if(this.sys.isAdmin) {
+        this.userDisplay = `${this.userDisplay}(A)`;
+      }
+    }
+  }
   
 }
