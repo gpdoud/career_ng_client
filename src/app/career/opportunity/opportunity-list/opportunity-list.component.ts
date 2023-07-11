b1767c138fb082233262f92f17f77e964f75251a
gpdoud <gpdoud@gmail.com>
2023-07-11 13:29:01 -0400

    Added "show inactive" on opportunity list

--- src/app/career/opportunity/opportunity-list/opportunity-list.component.ts
+++ src/app/career/opportunity/opportunity-list/opportunity-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges } from '@angular/core';
 import { OpportunityService } from '../opportunity.service';
 import { SystemService } from 'src/app/misc/services/system.service';
 import { Opportunity } from '../opportunity.class';
@@ -14,12 +14,17 @@ export class OpportunityListComponent {
   pageTitle = "Opportunity List";
   createRouterLink = "/opportunity/create";
   opportunites!: Opportunity[];
+  showInactive: boolean = false;
   get userIsAdmin() { return this.sys.isAdmin; }
 
   constructor(
     private sys: SystemService,
     private opsvc: OpportunityService
   ) { }
+
+  onCheckboxChange(event) {
+    this.refresh();
+  }
 
   searchCriteria: string = ''
   sortColumn: string = 'username'
@@ -48,23 +53,42 @@ export class OpportunityListComponent {
     }
   }
 
-  ngOnInit(): void {
-    this.sys.chkLogin();
+  filterInactives(opportunities: Opportunity[]): Opportunity[] {
+    if(this.showInactive) {
+      return opportunities;
+    }
+    let selected: Opportunity[] = [];
+    for(let o of opportunities) {
+      if(!o.active) {
+        continue;
+      }
+      selected.push(o);
+    }
+    return selected;
+  }
+
+  refresh(): void {
     let userId: number = 0;
     if (typeof this.sys.loggedInUser !== "undefined" && this.sys.loggedInUser !== null) {
       userId = this.sys.loggedInUser.id;
     }
     let observable = this.sys.isAdmin 
-      ? this.opsvc.list() 
-      : this.opsvc.studentList(userId);
+    ? this.opsvc.list() 
+    : this.opsvc.studentList(userId);
     observable.subscribe({
       next: (res) => {
+        res = this.filterInactives(res);
         this.addNames(res);
         console.debug("Opportunites", res);
         this.opportunites = res as Opportunity[];
       },
       error: (err) => console.error(err)
     });
+  }
+  
+  ngOnInit(): void {
+    this.sys.chkLogin();
+    this.refresh();
   }
 
 }
